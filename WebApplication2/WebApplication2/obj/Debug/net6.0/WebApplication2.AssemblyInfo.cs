9f29721d36387369a69d55fc87689abc50c9510b
22202573 umaydundar <113841709+umaydundar@users.noreply.github.com>
2024-07-21 17:10:14 +0300

    Stop tracking .vsidx files

--- WebApplication2/WebApplication2/obj/Debug/net6.0/WebApplication2.AssemblyInfo.cs
+++ WebApplication2/WebApplication2/obj/Debug/net6.0/WebApplication2.AssemblyInfo.cs
@@ -15,6 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0dfe60dca36b0ffb66ff79f304b53b2a77f1deb0")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
